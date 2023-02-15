8a66e7883ccbbc789667313ef53b691bd193466b
krupreecha <91063571+krupreecha@users.noreply.github.com>
2023-02-15 15:25:10 +0700

    Add files via upload

---
+++ Shorturl.php
@@ -0,0 +1,28 @@
+<?php
+class Shorturl 
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+    public function index()
+    {
+		
+    }
+    public function create()
+    {
+		
+    }
+    private function does_alias_exist($alias)
+    {
+		
+    }
+    private function save_new_alias($url, $alias)
+    {
+		
+    }
+    private function alias_from_url($url)
+    {
+
+	}
+}
